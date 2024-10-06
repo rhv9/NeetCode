174f33eaddb76736f2dce8c5a97eaf4f2a29a189
rhv9 <rahulhasmukv@gmail.com>
2024-10-06 12:17:38 +0100

    group_anagram Hashing a list by converting to tuple

--- ArrayAndHashing/group_anagrams.py
+++ ArrayAndHashing/group_anagrams.py
@@ -55,7 +55,70 @@ class SolutionIterative:
                 
         return output
 
+
+
+class SolutionCountHashed:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = []
+        anagramHash = {}
+
+        for str in strs:
+            strDict = {}
+            for c in str:
+                strDict[c] = strDict.get(c, 0) + 1
+            
+            found = False
+
+            anagramList = anagramHash.get(len(str), [])
+
+            for anagramTuple in anagramList:
+                anagramMatches = True
+                for c, count in strDict.items():
+                    if anagramTuple[0].get(c, 0) != count:
+                        anagramMatches = False
+                        break
+
+                if anagramMatches:
+                    # Found the anagram
+                    outputPointer = anagramTuple[1]
+                    output[outputPointer].append(str)
+                    found = True
+                    break
+            
+            if not found:
+                # add as sublist
+                outputSublist = [str]
+                output.append(outputSublist)
+                newAnagramTuple = (strDict, len(output) - 1)
+
+                if len(str) in anagramHash:
+                    anagramHash[len(str)].append(newAnagramTuple)
+                else:
+                    anagramHash[len(str)] = [newAnagramTuple]
+                
+        return output
+    
+class SolutionArrayHash:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = {}
+
+        for str in strs:
+            arrayDict = [0]*26
+
+            for c in str:
+                arrayDict[ord(c) - ord('a')] += 1
+            
+            tup = tuple(arrayDict)
+            if tup in output:
+                output[tup].append(str)
+            else:
+                output[tup] = [str]
+            
+        return output.values()
+
+
+
 strs=["act","pots","tops","cat","stop","hat"]
 
-solution = SolutionIterative.groupAnagrams({}, strs)
+solution = SolutionArrayHash.groupAnagrams({}, strs)
 print("Solution: ", solution)
